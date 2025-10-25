911a883de98f57a6731a3b49df6aa1d7eab15841
ZAIDALIKHAN <39004623+ZAIDALIKHAN@users.noreply.github.com>
2025-10-25 11:49:37 +0530

    Create KindSetUP.sh
    Kind Set up

---
+++ KindSetUP.sh
@@ -0,0 +1,39 @@
+#!/bin/bash/
+
+set -x
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
+
+sudo kind create cluster --name demo-cluster
+
+mkdir /home/ubuntu/.kube/
+sudo cp /root/.kube/config /home/ubuntu/.kube/config
+sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
+
+
+kubectl cluster-info
+kubectl get nodes
